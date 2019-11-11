ffde70ff5ef19d46dc0cf3c05fdf859856c51ee0
Simone Esposito <hi@simone.sh>
2019-11-11 14:56:41 +0100

    app: Add index.html with custom image URL

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from user_agents import parse
 
 app = Flask(__name__)
 
 def send_to_opa(device_brand):
     if device_brand == 'Apple':
-        return False
+        return "https://i.imgur.com/OD8x4U3.png"
     else:
-        return True
+        return "https://i.imgur.com/hvkdIxM.png"
 
 @app.route("/")
 def root():
     device_brand = parse(request.headers.get('User-Agent')).device.brand
     
     result = send_to_opa(device_brand)
-    if result:
-        return 'yes'
-    else:
-        return 'no'
+    return render_template('index.html', result=result)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
