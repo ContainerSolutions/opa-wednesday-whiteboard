590cb69a61aa9be72db3eea960e845c137510518
Simone Esposito <hi@simone.sh>
2019-11-13 13:25:33 +0100

    app: Use black for formatting

--- app.py
+++ app.py
@@ -14,13 +14,9 @@ app = Flask(__name__)
 opa_url = os.environ.get("OPA_ADDR", "http://localhost:8181")
 policy_path = os.environ.get("POLICY_PATH", "/v1/data/httpapi/authz")
 
+
 def check_auth(url, method, device_brand):
-    input_dict = {
-        "input": {
-            "device": device_brand,
-            "method": method,
-        }
-    }
+    input_dict = {"input": {"device": device_brand, "method": method,}}
 
     logging.info("Checking auth...")
     logging.info(json.dumps(input_dict, indent=2))
@@ -39,16 +35,18 @@ def check_auth(url, method, device_brand):
     logging.info(json.dumps(r.json(), indent=2))
     return r.json()
 
+
 @app.route("/")
 def root():
-    device_brand = parse(request.headers.get('User-Agent')).device.brand
-    print(parse(request.headers.get('User-Agent')))
+    device_brand = parse(request.headers.get("User-Agent")).device.brand
+    print(parse(request.headers.get("User-Agent")))
     url = opa_url + policy_path
 
     j = check_auth(url, request.method, device_brand).get("result", {})
     if j.get("allow", False) == True:
-        return render_template('index.html', result="https://i.imgur.com/O1VyTHW.mp4")
-    return render_template('index.html', result="https://i.imgur.com/DKUR9Tk.png")
-    
+        return render_template("index.html", result="https://i.imgur.com/O1VyTHW.mp4")
+    return render_template("index.html", result="https://i.imgur.com/DKUR9Tk.png")
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
